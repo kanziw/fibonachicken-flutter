e0932cd8e5e6b372a04e444ddcd4ca4111cf5cec
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-17 13:57:31 +0900

    Add github repository action

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_font_icons/flutter_font_icons.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,11 +11,20 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 2,
         title: const Text(
-          '피보나치킨',
+          '피보나치킨 계산기',
           style: TextStyle(
             fontSize: 24,
           ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await launchUrlString(
+                  'https://github.com/kanziw/fibonachicken-flutter');
+            },
+            icon: const Icon(MaterialCommunityIcons.github),
+          )
+        ],
       ),
     );
   }
