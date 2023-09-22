484bf031599bd3b969a0aabdeca33bb1c8c93a11
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-22 23:09:41 +0900

    Separate ExpandableAppendixWidget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:expandable/expandable.dart';
 import 'package:fibonachicken/fibonachicken.dart';
-import 'package:flutter/gestures.dart';
+import 'package:fibonachicken/widgets/expandable_appendix_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -29,12 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
-        title: const Text(
-          '피보나치킨 계산기',
-          style: TextStyle(
-            fontSize: 32,
-          ),
-        ),
+        title: const Text('피보나치킨 계산기', style: TextStyle(fontSize: 32)),
         toolbarHeight: 80,
         actions: [
           IconButton(
@@ -105,72 +99,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ],
                   ),
-                ExpandablePanel(
-                  header: const Text('세상 만사..'),
-                  collapsed: const Text(''),
-                  expanded: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                          '모든 것의 균형은 황금 비율에서 그 해답을 찾을 수 있고, 이를 수학적으로 풀어낸것이 바로 피보나치 수열이니라.'),
-                      RichText(
-                        text: const TextSpan(
-                          style: TextStyle(color: Colors.black),
-                          children: [
-                            TextSpan(
-                              text:
-                                  '일찍이 선지자가 있어, 치킨과 피보나치 수열의 관계를 밝힌 자들이 있으니 그들의 끝을 모르는 탐구 정신과 널리 인간과 치킨을 이롭게 하려는 마음을 높이 사, 내 잠시 짬을 내어 허접한 코드질을 하였으니 이를 ',
-                            ),
-                            TextSpan(
-                              text: '피보나치킨',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            TextSpan(
-                              text: ' 계산기라고 부르도록 하겠다.',
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: RichText(
-                          text: TextSpan(
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontStyle: FontStyle.italic,
-                            ),
-                            children: [
-                              const TextSpan(text: '원작자('),
-                              TextSpan(
-                                text: '@scari_net',
-                                recognizer: TapGestureRecognizer()
-                                  ..onTapDown = (details) async {
-                                    await launchUrlString(
-                                        'https://twitter.com/scari_net');
-                                  },
-                              ),
-                              const TextSpan(
-                                  text: ')의 동의를 얻어 게시하였습니다 :) from '),
-                              TextSpan(
-                                text: 'https://fibonachicken.herokuapp.com',
-                                recognizer: TapGestureRecognizer()
-                                  ..onTapDown = (details) async {
-                                    await launchUrlString(
-                                        'https://fibonachicken.herokuapp.com/');
-                                  },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  theme: const ExpandableThemeData(
-                    useInkWell: false,
-                    headerAlignment: ExpandablePanelHeaderAlignment.center,
-                    animationDuration: Duration(milliseconds: 50),
-                  ),
-                ),
+                const ExpandableAppendixWidget(),
               ],
             ),
           ),
