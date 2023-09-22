86451d3435c6f5700ad73b3a1039b93e6c8be2d4
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-22 23:28:53 +0900

    Make limit 10000

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -90,7 +90,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                       Text(
-                        peopleCount < 1000
+                        peopleCount < 10000
                             ? '🐔' * chickenCount
                             : '이정도면 🐔이 모자라지 않을까요...?',
                         style: const TextStyle(
